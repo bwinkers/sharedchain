cbe480252b18ac88170eb7e922fde58e8f335220
Brian Winkers <bwinkers@gmail.com>
2015-10-21 09:33:32 -0600

    Basic server.js

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 /**
  * SharedChain Server
  */
-// Don't allow sloppy coding.
 "use strict";
+
+/**
+ * Include the ActiveRules module
+ * This provides an opinionated means to create a Koa app.
+ * @type {exports|module.exports}
+ */
+var ar = require('./activerules');
+
+/**
+ * Define a directory that has the ActiveRule Configs
+ */
+ar.addRoute('/config');
+
+var app = ar.activeApp();
+
+/**
+ * Define the port you want the app to listen on
+ */
+app.listen(8888);
