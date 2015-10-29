de5035d672e3195ab566d3743e2af7efc2375806
Brian Winkers <bwinkers@gmail.com>
2015-10-28 22:49:10 -0600

    Working

---
+++ routes.js
@@ -0,0 +1,9 @@
+/**
+ * SharedChain Routes
+ *
+ * All routes for both the RESTful API and the WebSockets API are included
+ */
+"use strict"
+
+/**
+ *
--- server.js
+++ server.js
@@ -1,29 +1,85 @@
 /**
- * SharedChain Server
+ * SharedChain Link
+ *
+ * A Node.js application supporting the SharedChain Protocol
+ * http://sharedchain.com/protocol
  */
-"use strict";
+"use strict"
 
-var koa = require('koa');
-var koaws = require('koa-ws');
+var staticCache = require('koa-static-cache');
+var koa = require('koa.io');
+var path = require('path');
+var fs = require('fs');
+
 var app = koa();
 
-var options = {
-    serveClientFile: true,
-    clientFilePath: '/koaws.js',
-    heartbeat: true,
-    heartbeatInterval: 5000
-};
+var port = process.env.PORT || 3000;
 
-app.use(function *(){
+// Routing
+app.use(staticCache(path.join(__dirname, 'public')));
 
-    this.body = 'Hello World';
-    console.log('test2');
+app.use(function*() {
+    this.body = fs.createReadStream(path.join(__dirname, 'public/index.html'));
+    this.type = 'html';
 });
 
-app.use(koaws(app, options));
-
-app.listen(3000);
-
-app.ws.register('hello', function* () {
-    this.result('world!');
+app.listen(port, function () {
+    console.log('Server listening at port %d', port);
 });
+
+// Chatroom
+
+// links which are currently connected to the chat
+var links = {};
+var numLinks = 0;
+
+// middleware for connect and disconnect
+app.io.use(function* linkLeft(next) {
+    // on connect
+    console.log('link connected');
+    console.log(this.headers)
+    yield* next;
+    // on disconnect
+    if (this.addedLink) {
+        delete links[this.link];
+        --numLinks;
+
+        // echo globally that this client has left
+        this.broadcast.emit('link left', {
+            link: this.link,
+            numLinks: numLinks
+        });
+    }
+});
+
+
+/**
+ * router for socket event
+ */
+
+app.io.route('add link', function* (next, link) {
+    // we store the link in the socket session for this client
+    this.link = link;
+    // add the client's link to the global list
+    links[link] = link;
+    ++numLinks;
+    this.addedLink = true;
+    this.emit('login', {
+        numLinks: numLinks
+    });
+
+    // echo globally (all clients) that a person has connected
+    this.broadcast.emit('link joined', {
+        link: this.link,
+        numLinks: numLinks
+    });
+});
+
+// when the client emits 'new message', this listens and executes
+app.io.route('new message', function* (next, message) {
+    // we tell the client to execute 'new message'
+    this.broadcast.emit('new message', {
+        link: this.link,
+        message: message
+    });
+})
