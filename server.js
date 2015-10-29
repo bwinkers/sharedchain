6c80c056c60c670db45444d829e041795427c7c0
Brian Winkers <bwinkers@gmail.com>
2015-10-28 23:51:24 -0600

    still Working

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /**
- * SharedChain Link
+ * SharedChain Chainlink
  *
  * A Node.js application supporting the SharedChain Protocol
  * http://sharedchain.com/protocol
@@ -8,46 +8,56 @@
 
 var staticCache = require('koa-static-cache');
 var koa = require('koa.io');
+var router = require('koa-router')();
 var path = require('path');
 var fs = require('fs');
 
+// Create a basic Koa app
 var app = koa();
 
+// Set the port this Chainlink will run on
 var port = process.env.PORT || 3000;
 
 // Routing
 app.use(staticCache(path.join(__dirname, 'public')));
 
-app.use(function*() {
+// Homepage route
+router.get('/', function *(next) {
     this.body = fs.createReadStream(path.join(__dirname, 'public/index.html'));
     this.type = 'html';
 });
 
+// Use our REST API and HTML Content routes
+app.use(router.routes())
+    .use(router.allowedMethods());
+
+// Start Listenting
+// Don't use WebSockets prior to this
 app.listen(port, function () {
     console.log('Server listening at port %d', port);
 });
 
-// Chatroom
+// Chainlinks
 
-// links which are currently connected to the chat
-var links = {};
-var numLinks = 0;
+// chainlinks which are currently connected to the SharedChaint
+var chainlinks = {};
+var numChainlinks = 0;
 
 // middleware for connect and disconnect
-app.io.use(function* linkLeft(next) {
+app.io.use(function* chainlinkLeft(next) {
     // on connect
-    console.log('link connected');
+    console.log('chainlink connected');
     console.log(this.headers)
     yield* next;
     // on disconnect
-    if (this.addedLink) {
-        delete links[this.link];
-        --numLinks;
+    if (this.addedChainlink) {
+        delete chainlinks[this.chainlink];
+        --numChainlinks;
 
         // echo globally that this client has left
-        this.broadcast.emit('link left', {
-            link: this.link,
-            numLinks: numLinks
+        this.broadcast.emit('chainlink left', {
+            chainlink: this.chainlink,
+            numChainlinks: numChainlinks
         });
     }
 });
@@ -57,21 +67,21 @@ app.io.use(function* linkLeft(next) {
  * router for socket event
  */
 
-app.io.route('add link', function* (next, link) {
-    // we store the link in the socket session for this client
-    this.link = link;
-    // add the client's link to the global list
-    links[link] = link;
-    ++numLinks;
-    this.addedLink = true;
+app.io.route('add chainlink', function* (next, chainlink) {
+    // we store the chainlink in the socket session for this client
+    this.chainlink = chainlink;
+    // add the client's chainlink to the global list
+    chainlinks[chainlink] = chainlink;
+    ++numChainlinks;
+    this.addedChainlink = true;
     this.emit('login', {
-        numLinks: numLinks
+        numChainlinks: numChainlinks
     });
 
-    // echo globally (all clients) that a person has connected
-    this.broadcast.emit('link joined', {
-        link: this.link,
-        numLinks: numLinks
+    // echo globally (all clients) that a chainlink has connected
+    this.broadcast.emit('chainlink joined', {
+        chainlink: this.chainlink,
+        numChainlinks: numChainlinks
     });
 });
 
@@ -79,7 +89,7 @@ app.io.route('add link', function* (next, link) {
 app.io.route('new message', function* (next, message) {
     // we tell the client to execute 'new message'
     this.broadcast.emit('new message', {
-        link: this.link,
+        chainlink: this.chainlink,
         message: message
     });
 })
