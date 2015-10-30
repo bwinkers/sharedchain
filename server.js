0a786d62ebc583d54cb136b30c792a02ae4f17b7
Brian Winkers <bwinkers@gmail.com>
2015-10-29 22:43:20 -0600

    Good to go, no semi-colons

--- server.js
+++ server.js
@@ -7,29 +7,29 @@
 "use strict"
 
 // Set the port this Chainlink will run on
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000
 
-const koa = require('koa'),
-    route = require('koa-route'),
-    websockify = require('koa-websocket');
+const koa = require('koa')
+const route = require('koa-route')
+const websockify = require('koa-websocket')
 
-const app = websockify(koa());
+const app = websockify(koa())
 
 app.use(route.get('/', function* (next) {
-    this.body = 'Heello';
-}));
+    this.body = 'Heello'
+}))
 
 // Note it's app.ws.use and not app.use
 app.ws.use(route.all('/', function* (next) {
     // `this` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
     // the websocket is added to the context on `this.websocket`.
-    this.websocket.send('Hello World');
+    this.websocket.send('Hello World')
     this.websocket.on('message', function(message) {
         // do something with the message from client
-        console.log(message);
+        console.log(message)
     });
     // yielding `next` will pass the context (this) on to the next ws middleware
-    yield next;
-}));
+    yield next
+}))
 
-app.listen(port);
+app.listen(port)
