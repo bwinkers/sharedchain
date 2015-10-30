17bc7cb18549a78d476e6d0a815301891425e61f
Brian Winkers <bwinkers@gmail.com>
2015-10-30 10:20:55 -0600

    cleaned ip code

--- server.js
+++ server.js
@@ -9,24 +9,37 @@
 // Set the port this Chainlink will run on
 var port = process.env.PORT || 3000
 
+// Koa base components
 const koa = require('koa')
 const route = require('koa-route')
-const websockify = require('koa-websocket')
 
+// WebSocket via ws
+const websockify = require('koa-websocket')
 const app = websockify(koa())
 
+// View Templates
 const co = require('co')
 const views = require('co-views')
-
 var render = views('views', {
     map: { html: 'nunjucks' }
 });
 
+// Static files
+const path = require('path')
+const staticCache = require('koa-static-cache')
+app.use(staticCache(path.join(__dirname, 'static'), {
+    maxAge: 365 * 24 * 60 * 60
+}))
+
+// Both routing sections below will call a function and use one `.use` for all routes of that type.
+// That will keep the stack smaller.
+
+// REST and Website Routes
 app.use(route.get('/', function* () {
     this.body = yield render('home.html', {testa:5})
 }))
 
-// Note it's app.ws.use and not app.use
+// WebSocket Routes
 app.ws.use(route.all('/', function* (next) {
     // `this` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
     // the websocket is added to the context on `this.websocket`.
@@ -39,4 +52,5 @@ app.ws.use(route.all('/', function* (next) {
     yield next
 }))
 
+// Turn it all on
 app.listen(port)
